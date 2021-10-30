0bfbd856d3dc2632e43ba150f17be0b2ef9d40f3
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-10-30 23:41:10 +0545

    Added All

--- src/component/Basics/MenuCard.js
+++ src/component/Basics/MenuCard.js
@@ -6,17 +6,19 @@ function MenuCard({menuData}) {
         <>
             <section className ="main-card--cointainer">
             {menuData.map((curElem) =>{
+
+                const {id, name, category, image, description} = curElem;
                 return(
-                        <div className="card-container" key={curElem.id}>
+                        <div className="card-container" key={id}>
                             <div className="card">
                                 <div className="card-body">
-                                    <span className="card-number card-cirlce subtle">{curElem.id}</span>
-                                    <span className="card-author subtle">{curElem.name}</span>
+                                    <span className="card-number card-cirlce subtle">{id}</span>
+                                    <span className="card-author subtle">{name}</span>
                                     <h2 className="card-title">{curElem.name}</h2>
-                                    <span className="card-description subtle">{curElem.description}</span>
+                                    <span className="card-description subtle">{description}</span>
                                     <div className="card-read">Read</div>                           
                                 </div>
-                                <img src={curElem.image} alt="images" className="card-media"/>
+                                <img src={image} alt="images" className="card-media"/>
             
                                 <span className="card-tag subtle">Order Now</span>
                             </div>
