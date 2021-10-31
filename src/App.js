b514333cc19cb068b85e5c4db97722f88863fd28
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-10-31 20:26:29 +0545

    Added: Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Resturant from "./component/Basics/Resturant";
-
+import UseState from "./component/Hooks/useState";
+import UseEffect from "./component/Hooks/useEffect";
+import UseReducer from './component/Hooks/useReducer';
 const App = () => {
-  return (
-    <div>
-      <Resturant/>
-    </div>
-  )
+  return <UseReducer/>;
 }
 
 export default App;
